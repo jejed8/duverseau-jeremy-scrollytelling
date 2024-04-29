5af14e99fc6e4e88d041b85152609cbc635c60bb
jejed8 <47800238+jejed8@users.noreply.github.com>
2024-04-29 11:21:09 -0400

    p2.2

--- site-scrolly/script/script.js
+++ site-scrolly/script/script.js
@@ -1,12 +1,24 @@
-
+//----- GSAP -----
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
   // gsap code here!
-  gsap.to('#CTA', {
+  gsap.to("#CTA", {
     y: 100,
     duration: 2.5,
-    repeat: '-1',
+    repeat: "-1",
   });
-
 });
 
+
+//----- Sprite Sheet -----
+let scrollTimer;
+
+function removeScrollingClass() {
+    document.body.classList.remove('is-scrolling');
+}
+
+window.addEventListener("scroll", function () {
+  document.body.classList.add("is-scrolling");
+  clearTimeout(scrollTimer);
+  scrollTimer = setTimeout(removeScrollingClass, 100);
+});
